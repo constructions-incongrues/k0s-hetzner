0a0bb9e8cb3fde2d2f266d9e7efb26860cdab3a1
Tristan Rivoallan <tristan@rivoallan.net>
2024-01-29 22:21:03 +0000

    typo

--- stacks/marmite/output.tf
+++ stacks/marmite/output.tf
@@ -3,6 +3,11 @@ output "kubeconfig" {
   sensitive = true
 }
 
+output "kubeconfig_filename" {
+  value = module.marmite.kubeconfig_filename
+  sensitive = true
+}
+
 output "load_balancer_ipv4_address" {
   value = module.marmite.load_balancer_ipv4_address
 }
