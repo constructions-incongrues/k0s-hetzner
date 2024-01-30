28ca1beb4100918d975d37f8282811a5ef615950
Tristan Rivoallan <tristan@rivoallan.net>
2024-01-30 02:19:20 +0000

    yo

--- modules/k0s-cluster/modules/ssh_keys/output.tf
+++ modules/k0s-cluster/modules/ssh_keys/output.tf
@@ -3,10 +3,10 @@ output "name" {
 }
 
 output "public_key" {
-  value = module.ssh-keypair-generator.public_key
+  value = module.ssh-keypair-generator.keypair.public_key_openssh
 }
 
 output "private_key" {
-  value = module.ssh-keypair-generator.private_key
+  value = module.ssh-keypair-generator.keypair.private_key_openssh
   sensitive = true
 }
