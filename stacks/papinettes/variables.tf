7c5db4a4b1d793e7fe8b83d5773cde8dcdf58048
Tristan Rivoallan <tristan@rivoallan.net>
2024-01-30 00:51:22 +0000

    yo

---
+++ stacks/papinettes/variables.tf
@@ -0,0 +1,12 @@
+variable "hcloud_api_token" {
+  description = "Hetzner API token"
+  type        = string
+}
+
+variable "cloudflare_email" {
+  type = string
+}
+
+variable "cloudflare_api_key" {
+  type = string
+}
